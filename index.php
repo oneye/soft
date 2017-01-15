fb78667618ae09c8f104c9e1184c3e387bfe9ce5
Lars Knickrehm <mail@lars-sh.de>
2017-01-15 17:18:55 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: text/plain');
+echo '<eyePorts>
+	<portServer>
+		<address>soft.oneye-project.org</address>
+		<name>eyeSoft Repository</name>
+		<path>/packages.xml</path>
+		<version>1.11</version>
+	</portServer>
+</eyePorts>';
+?>
